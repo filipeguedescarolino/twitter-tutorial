587c88945e802837613b290dca6b7dc0bbc7b20c
1190100 <1190100@isep.ipp.pt>
2021-01-20 12:44:44 +0000

    single-page functionalities

---
+++ src/components/userProfile.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{user.userName}}</h1>
+            <div class="user-profile__follower-count"> 
+                <strong> Followers: </strong> {{followers}}
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        userName: 'FilipeCarolino',
+        firstName: 'Filipe',
+        lastName: 'Guedes',
+        email: 'filipe.guedes@gmail.com',
+        isAdmin: true
+
+      }
+    }
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.userName} has gained a new follower`)
+      } 
+    }
+  },
+
+
+  computed: {
+    fullName() {
+      return this.user.firstName +  this.user.lastName
+    }
+  },
+  
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  
+  },
+  mounted() {
+    this.followUser();
+  }
+  
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
